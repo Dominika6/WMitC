745c2264bd85365122b09810b229e2547ee8325b
Dominika6 <dominikajadach@o2.pl>
2022-04-27 14:54:28 +0200

    position and supervisor choosing

--- frontend/src/components/Admin/CreateUser.js
+++ frontend/src/components/Admin/CreateUser.js
@@ -1,52 +1,66 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Typography, Container } from "@material-ui/core";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from "react-router-dom";
-import { Button, FormControl, InputGroup } from "react-bootstrap";
+import { Form, Button, FormControl, InputGroup } from "react-bootstrap";
 
 import { createUser } from "../../actions/users";
 import Input from "../Auth/Input";
+import { getAllUsers } from "../../actions/users";
 
 
 const initialState = {
-    supervisor: 'coordinator@gmail.com',
+    supervisor: '',
     firstName: '',
     lastName: '',
     phoneNumber: '',
     email: '',
     password: '',
     confirmPassword: '',
-    position: 'user'
+    position: ''
 }
 
-// const positions = [
-//     { value: 'admin', label: 'Administrator' },
-//     { value: 'manager', label: 'Coordinator' },
-//     { value: 'user', label: 'Worker' }
-// ];
+const positions = [
+    { value: 'admin', label: 'Administrator' },
+    { value: 'manager', label: 'Coordinator' },
+    { value: 'user', label: 'Worker' }
+];
 
-//https://dev.to/helmarjunior/using-react-select-async-with-react-hooks-and-pagination-22g2 -> TODO WYBIERANIE
-
-// TODO po zrobieniu usera brak przekierowania (logowania ) i wyświetlenie danych do logowania ( do przekazania dla usera )
 
 const CreateUser = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState(initialState);
+    const [currentId] = useState(0);
     const dispatch = useDispatch();
     const history = useHistory();
+    const { users, isLoading } = useSelector((state) => state.users);
+    const allUsers = [];
+
+    useEffect(() => {
+        dispatch(getAllUsers())
+    }, [currentId, dispatch]);
+
+    isLoading === false ?
+        users.map((user) => (
+            allUsers.push(user)
+        ))
+        : (console.log())
 
     const handleShowPassword = () => setShowPassword((prevShowPassword) => ! prevShowPassword);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(formData);
+        if(formData.position === 'admin'){
+            formData.supervisor = 'none';
+        }
         dispatch(createUser(formData, history));
     };
 
     const handleChange = (e) => {
         setFormData({...formData, [e.target.name]: e.target.value});
     };
-    //todo po zrobieniu konta użytkownika pozostać na stronie, wyczyścić formularz i wyświetlić info, że został utworzony
+    //todo po zrobieniu konta użytkownika pozostać na stronie, wyczyścić formularz i wyświetlić info, że został utworzony i dane do logowania ( do przekazania dla usera )
 
     return(
         <Container component="main" maxWidth="xs"><br/><br/>
@@ -82,7 +96,28 @@ const CreateUser = () => {
                     <Input name="confirmPassword" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
                 </InputGroup><br/>
 
-                {/*TODO Na razie koordynatora i pozycję dodajemy na sztywno - zrobić select */}
+                <Typography>{'Position'}</Typography>
+                <Form.Group className="mb-3" controlId="formBasicSelect">
+                    <Form.Control as="select" name="position" onChange={handleChange}>
+                        {positions.map((position) => <option value={position.value} key={position.value}>{position.label}</option> )}
+                    </Form.Control>
+                </Form.Group>
+
+                {formData.position === 'manager' ?
+                <>
+                    <Typography>{'Supervisor'}</Typography>
+                    <Form.Control as="select" name="supervisor" onChange={handleChange}>
+                        {allUsers.map((user) => (user.position === 'admin') ? <option value={user.email} key={user.email}>{user.name}</option> : <></> )}
+                    </Form.Control><br/>
+                </> : (formData.position === 'user') ?
+                <>
+                    <Typography>{'Supervisor'}</Typography>
+                    <Form.Control as="select" name="supervisor" onChange={handleChange}>
+                        {allUsers.map((user) => (user.position === 'manager') ? <option value={user.email} key={user.email}>{user.name}</option> : <></> )}
+                    </Form.Control><br/>
+                </> : (formData.position === 'admin') ? <></>:<></>
+                }
+
                 <div className="d-grid gap-2">
                     <Button type="submit" variant="success" >
                         { 'Create User' }
