e866e94c385f0ff4d906d6b325a4d0cd0f52a2a2
Dominika6 <dominikajadach@o2.pl>
2022-05-06 21:29:39 +0200

    done todos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar1 from "./components/Navbar/Navbar1";
@@ -23,7 +23,7 @@ import MyTeamCoordinator from "./components/Coordinator/MyTeamCoordinator";
 
 const App = () => {
 
-    //todo - pozwalanie na otwieranie danych stron tylko dla uprawnionych - auth
+    const [loggedUser] = useState(JSON.parse(localStorage.getItem("profile")));
 
     return(
         <BrowserRouter>
@@ -31,24 +31,25 @@ const App = () => {
                 <Navbar1/>
                 <br/><br/><br/><br/><br/>
                 <Switch>
+
                     <Route path="/" exact component={About} />
-                    <Route path="/task/:id" exact component={TaskDetails} />
                     <Route path="/login" exact component={Login} />
-                    <Route path="/myAccount" exact component={MyAccount} />
-                    <Route path="/createUser" exact component={CreateUser} />
-                    {/*<Route path="/createUser" exact component={() => (!user && !(user?.result.position === "admin") ? <CreateUser /> : <Redirect to='/' />)} />*/}
-                    <Route path="/createClient" exact component={CreateClient} />
-                    {/*<Route path="/createClient" exact component={() => (!user && !(user?.result.position === "admin") ? <CreateClient /> : <Redirect to='/' />)} />*/}
-                    <Route path="/createProject" exact component={CreateProject} />
-                    {/*<Route path="/createProject" exact component={() => (!user && !(user?.result.position === "manager") ? <CreateProject /> : <Redirect to='/' />)} />*/}
-                    <Route path="/createTask" exact component={CreateTask} />
-                    <Route path="/getMyTasks" exact component={WorkerHome} />
-                    <Route path="/getMyTeam" exact component={TeamHome} />
-                    <Route path="/getMyTeamC" exact component={MyTeamCoordinator} />
-                    <Route path="/getMyClients" exact component={ClientsHomeCoordinator} />
-                    <Route path="/getAllUsers" exact component={UsersHome} />
-                    <Route path="/getAllClients" exact component={ClientsHomeAdmin} />
-                    <Route path="/getAllTasksC" exact component={TasksHomeCoordinator} />
+                    <Route path="/task/:id" exact component={() => (loggedUser ? <TaskDetails/> : <Redirect to='/' />)} />
+                    <Route path="/myAccount" exact component={() => (loggedUser ? <MyAccount/> : <Redirect to='/' />)} />
+
+                    <Route path="/createUser" exact component={() => (loggedUser?.result?.position === 'admin' ? <CreateUser/> : <Redirect to='/' />)} />
+                    <Route path="/getAllUsers" exact component={() => (loggedUser?.result?.position === 'admin' ? <UsersHome/> : <Redirect to='/' />)} />
+                    <Route path="/createClient" exact component={() => (loggedUser?.result?.position === 'admin' ? <CreateClient/> : <Redirect to='/' />)} />
+                    <Route path="/getAllClients" exact component={() => (loggedUser?.result?.position === 'admin' ? <ClientsHomeAdmin/> : <Redirect to='/' />)} />
+
+                    <Route path="/createTask" exact component={() => (loggedUser?.result?.position === 'manager' ? <CreateTask/> : <Redirect to='/' />)} />
+                    <Route path="/getMyTeamC" exact component={() => (loggedUser?.result?.position === 'manager' ? <MyTeamCoordinator/> : <Redirect to='/' />)} />
+                    <Route path="/getMyClients" exact component={() => (loggedUser?.result?.position === 'manager' ? <ClientsHomeCoordinator/> : <Redirect to='/' />)} />
+                    <Route path="/getAllTasksC" exact component={() => (loggedUser?.result?.position === 'manager' ? <TasksHomeCoordinator/> : <Redirect to='/' />)} />
+                    <Route path="/createProject" exact component={() => (loggedUser?.result?.position === 'manager' ? <CreateProject/> : <Redirect to='/' />)} />
+
+                    <Route path="/getMyTeam" exact component={() => (loggedUser?.result?.position === 'user' ? <TeamHome/> : <Redirect to='/' />)} />
+                    <Route path="/getMyTasks" exact component={() => (loggedUser?.result?.position === 'user' ? <WorkerHome/> : <Redirect to='/' />)} />
 
                 </Switch>
             </Container>
